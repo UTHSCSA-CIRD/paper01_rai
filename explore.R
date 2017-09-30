93733d88c02e50f5184bef1f0ee8657db0fdeb77
Alex F. Bokov, Ph.D <alex.bokov@gmail.com>
2017-09-30 09:53:30 -0500

    explore.R created

---
+++ explore.R
@@ -0,0 +1,15 @@
+#' ---
+#' title: "RAI Exploratoru Tables and Plots"
+#' author: "Wilson, Bokov, Shireman"
+#' date: "09/30/2017"
+#' ---
+#' 
+#' After `run.R` has completed, we do whatever
+#' summary tables and plots in this script. All
+#' the libraries and `config.R` variables are taken
+#' care of by `run.R`
+#if('clearenv'%in% ls()) clearenv():
+
+source('run.R');
+
+
