503932792757545bb9ede3a7ea087038a0cb727b
Desiree Wilson <wilsond3@uthscsa.edu>
2017-10-19 10:32:03 -0500

    I caught a bug in the 'DataVisualization.R' file. I mistakenly used the 'summarize' function and plotted the results from this function in the subsequent boxplots. I corrected the issue now.

--- DataVisualization.R
+++ DataVisualization.R
@@ -1,10 +1,36 @@
+#calling 'run.R' so that I can load the dataset. 'run.R' also does minor data cleaning.
 source('./run.R');
 
+
+#exploring the relationship between income and frailty incidence in all colectomy patients
+#that have a Clavien-Dindo Grade 4 complication (TRUE) or not (FALSE)
 dat1subs[["all_colon_all"]] %>% select(a_discrete_rai,hispanic_ethnicity, a_any_cd4, income_final) %>% group_by( a_discrete_rai,hispanic_ethnicity, a_any_cd4) %>% 
-   # the mean of a T/F variable is the percent TRUE
-   summarise(income=IQR(income_final, na.rm=TRUE)) %>%
-   ggplot(aes(x=a_discrete_rai,y=income,fill=a_any_cd4)) + 
-   geom_boxplot() -> plot_any_cd4;
+   ggplot(aes(x=a_discrete_rai,y=income_final,fill=a_any_cd4)) + 
+   geom_boxplot() + labs(title = "Income Vs Frailty Vs Clavien-Dindo Grade 4 Complications in All Colectomy Patients") +
+   scale_fill_discrete(name="Clavien-Dindo Grade4"
+                      ,breaks=c("FALSE", "TRUE")
+                      ,labels=c("No", "Yes")
+                    ) -> plot_any_cd4;
  
 
- 
+#selecting patients that DO NOT have Clavien-Dindo Grade 4 complications
+dat1subs[["all_colon_all"]] %>% select(a_discrete_rai,hispanic_ethnicity, income_final, a_any_cd4) %>% group_by( a_discrete_rai,hispanic_ethnicity) %>% 
+  filter(a_any_cd4=='FALSE') %>%
+  ggplot(aes(x=a_discrete_rai,y=income_final,fill=hispanic_ethnicity)) + 
+  geom_boxplot() + labs(title = "Income Vs Frailty Vs Hispanic Ethnicity in All Colectomy Patients with No CD4 Complications") +
+  scale_fill_discrete(name="Ethnicity"
+                     ,breaks=c("No", "Unknown", "Yes")
+                     ,labels=c("Non-Hispanic", "Unknown", "Hispanic")
+                    )-> plot_no_cd4;
+
+
+#selecting patients that HAVE Clavien-Dindo Grade 4 complications
+dat1subs[["all_colon_all"]] %>% select(a_discrete_rai,hispanic_ethnicity, income_final, a_any_cd4) %>% group_by( a_discrete_rai,hispanic_ethnicity) %>% 
+  filter(a_any_cd4=='TRUE') %>%
+  ggplot(aes(x=a_discrete_rai,y=income_final,fill=hispanic_ethnicity)) + 
+  geom_boxplot() + labs(title = "Income Vs Frailty Vs Hispanic Ethnicity in All Colectomy Patients with CD4 Complications") +
+  scale_fill_discrete(name="Ethnicity"
+                     ,breaks=c("No", "Unknown", "Yes")
+                     ,labels=c("Non-Hispanic", "Unknown", "Hispanic")
+                     )-> plot_cd4;
+
