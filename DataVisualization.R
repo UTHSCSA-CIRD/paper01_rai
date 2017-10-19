7da8425de1cc58678c81abd9a74a3297028641f1
Desiree Wilson <wilsond3@uthscsa.edu>
2017-10-19 10:56:49 -0500

    Included a path to write the data analysis graphs to.

--- DataVisualization.R
+++ DataVisualization.R
@@ -34,3 +34,8 @@ dat1subs[["all_colon_all"]] %>% select(a_discrete_rai,hispanic_ethnicity, income
                      ,labels=c("Non-Hispanic", "Unknown", "Hispanic")
                      )-> plot_cd4;
 
+pdf(height = 11, width = 8.5, onefile = TRUE, file = paste0(outputpath,"CD4complications.pdf"))
+plot_any_cd4
+plot_no_cd4
+plot_cd4
+dev.off()
