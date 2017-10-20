5163f3031aeac4585e8dbf4f2e78471079e9eabb
Alex F. Bokov <bokov@uthscsa.edu>
2017-10-20 14:50:54 -0500

    Added markdown-able comments, updated invokation of run.R and global.R

--- DataVisualization.R
+++ DataVisualization.R
@@ -1,9 +1,11 @@
-#calling 'run.R' so that I can load the dataset. 'run.R' also does minor data cleaning.
-source('./run.R');
+#' calling 'run.R' so that I can load the dataset. 'run.R' also does minor data cleaning.
+#+ echo=FALSE, message=FALSE
+source('global.R');
+#+ cache=TRUE, echo=FALSE, message=FALSE
+source('run.R');
 
-
-#exploring the relationship between income and frailty incidence in all colectomy patients
-#that have a Clavien-Dindo Grade 4 complication (TRUE) or not (FALSE)
+#' exploring the relationship between income and frailty incidence in all colectomy patients
+#' that have a Clavien-Dindo Grade 4 complication (TRUE) or not (FALSE)
 dat1subs[["all_colon_all"]] %>% select(a_discrete_rai,hispanic_ethnicity, a_any_cd4, income_final) %>% group_by( a_discrete_rai,hispanic_ethnicity, a_any_cd4) %>% 
    ggplot(aes(x=a_discrete_rai,y=income_final,fill=a_any_cd4)) + 
    geom_boxplot() + labs(title = "Income Vs Frailty Vs Clavien-Dindo Grade 4 Complications in All Colectomy Patients") +
@@ -13,7 +15,7 @@ dat1subs[["all_colon_all"]] %>% select(a_discrete_rai,hispanic_ethnicity, a_any_
                     ) -> plot_any_cd4;
  
 
-#selecting patients that DO NOT have Clavien-Dindo Grade 4 complications
+#' selecting patients that DO NOT have Clavien-Dindo Grade 4 complications
 dat1subs[["all_colon_all"]] %>% select(a_discrete_rai,hispanic_ethnicity, income_final, a_any_cd4) %>% group_by( a_discrete_rai,hispanic_ethnicity) %>% 
   filter(a_any_cd4=='FALSE') %>%
   ggplot(aes(x=a_discrete_rai,y=income_final,fill=hispanic_ethnicity)) + 
@@ -34,8 +36,8 @@ dat1subs[["all_colon_all"]] %>% select(a_discrete_rai,hispanic_ethnicity, income
                      ,labels=c("Non-Hispanic", "Unknown", "Hispanic")
                      )-> plot_cd4;
 
-pdf(height = 11, width = 8.5, onefile = TRUE, file = paste0(outputpath,"CD4complications.pdf"))
-plot_any_cd4
-plot_no_cd4
-plot_cd4
-dev.off()
+# pdf(height = 11, width = 8.5, onefile = TRUE, file = paste0(outputpath,"CD4complications.pdf"))
+# plot_any_cd4
+# plot_no_cd4
+# plot_cd4
+# dev.off()
