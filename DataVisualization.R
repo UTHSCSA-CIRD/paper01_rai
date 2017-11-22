49884007aaedaaa7e1fe90a16333ce87cb63804c
Desiree Wilson <wilsond3@uthscsa.edu>
2017-11-22 12:44:43 -0600

    I updated the file again to include violin plots.

--- DataVisualization.R
+++ DataVisualization.R
@@ -7,7 +7,7 @@ source('./run.R');
 thedata <- dat1subs[["all_colon_all"]] %>% select(a_discrete_rai, a_any_cd4, income_final) %>% group_by( a_discrete_rai, a_any_cd4)
 ggplot(data = thedata, aes(x = factor(a_discrete_rai)
                           ,y = income_final,fill=a_any_cd4)) + 
-geom_boxplot(na.rm = TRUE) + labs(title = "Income Vs Frailty Vs Clavien-Dindo Grade 4 Complications in All Colectomy Patients") +
+geom_boxplot(na.rm = TRUE) + labs(title = "Income Vs Frailty Vs Clavien-Dindo Grade 4 Complications in All UHS Colectomy Patients") +
 scale_fill_discrete(name = "Clavien-Dindo Grade4"
                     ,breaks = c("FALSE", "TRUE")
                     ,labels = c("No", "Yes")
@@ -24,7 +24,7 @@ thedata2 <- dat1subs[["all_colon_all"]] %>% select(a_discrete_rai,hispanic_ethni
             filter(a_any_cd4=='FALSE')
 ggplot(data = thedata2, aes(x = a_discrete_rai
                            ,y = income_final, fill = hispanic_ethnicity)) + 
-geom_boxplot() + labs(title = "Income Vs Frailty Vs Hispanic Ethnicity in All Colectomy Patients with No CD4 Complications") +
+geom_boxplot() + labs(title = "Income Vs Frailty Vs Hispanic Ethnicity in All UHS Colectomy Patients with No CD4 Complications") +
 scale_fill_discrete(name = "Ethnicity"
                     ,breaks = c("No", "Unknown", "Yes")
                     ,labels = c("Non-Hispanic", "Unknown", "Hispanic")
@@ -41,7 +41,7 @@ plot_no_cd4 + annotate("text", x = c(0.75, 1.0, 1.25, 1.8, 2.2, 3.0)
 thedata3 <- dat1subs[["all_colon_all"]] %>% select(a_discrete_rai,hispanic_ethnicity, income_final, a_any_cd4) %>% group_by( a_discrete_rai,hispanic_ethnicity) %>% 
             filter(a_any_cd4=='TRUE')
 ggplot(data = thedata3, aes(x = a_discrete_rai, y = income_final,fill=hispanic_ethnicity)) + 
-geom_boxplot() + labs(title = "Income Vs Frailty Vs Hispanic Ethnicity in All Colectomy Patients with CD4 Complications") +
+geom_boxplot() + labs(title = "Income Vs Frailty Vs Hispanic Ethnicity in All UHS Colectomy Patients with CD4 Complications") +
 scale_fill_discrete(name = "Ethnicity"
                     ,breaks = c("No", "Unknown", "Yes")
                     ,labels = c("Non-Hispanic", "Unknown", "Hispanic")
@@ -60,7 +60,7 @@ thedata4 <- dat1subs[["all_colon_all"]] %>% select(a_discrete_rai,hispanic_ethni
 ggplot(data = thedata4, aes(x = a_discrete_rai
                             ,y = income_final, fill = a_any_cd4)) + 
   geom_boxplot() + 
-  labs(title = "Income Vs Frailty Vs CD4 Complications in Non-Hispanic Colectomy Patients") +
+  labs(title = "Income Vs Frailty Vs CD4 Complications in Non-Hispanic UHS Colectomy Patients") +
   scale_fill_discrete(name = "Clavien-Dindo Grade4"
                       ,breaks = c("FALSE", "TRUE")
                       ,labels = c("No", "Yes")
@@ -79,7 +79,7 @@ thedata5 <- dat1subs[["all_colon_all"]] %>% select(a_discrete_rai,hispanic_ethni
 ggplot(data = thedata5, aes(x = a_discrete_rai
                             ,y = income_final, fill = a_any_cd4)) + 
   geom_boxplot() + 
-  labs(title = "Income Vs Frailty Vs CD4 Complications in Hispanic Colectomy Patients") +
+  labs(title = "Income Vs Frailty Vs CD4 Complications in UHS Hispanic Colectomy Patients") +
   scale_fill_discrete(name = "Clavien-Dindo Grade4"
                       ,breaks = c("FALSE", "TRUE")
                       ,labels = c("No", "Yes")
@@ -93,7 +93,7 @@ plot_no_cd4_hisp + annotate("text", x = c(0.75, 1.2, 1.8, 2.2, 2.8, 3.2)
                             ,size = 5)
 
 
-pdf(height = 10, width = 7.5, onefile = TRUE, file = paste0(outputpath,"UHS_ACSNSQIP_CD4complications-DSW-", format(Sys.Date(), '%m-%d-%Y'),".pdf"))
+pdf(height = 10, width = 7.5, onefile = TRUE, file = paste0(outputpath,"UHS_ACSNSQIP_CD4comps_boxplots-DSW-", format(Sys.Date(), '%m-%d-%Y'),".pdf"))
 plot_any_cd4 + annotate("text", x = c(0.8, 1.2, 1.8, 2.2, 2.8, 3.2)
                         ,y = 10000, label = as.character(thecounts$n)
                         ,size = 6)
@@ -107,6 +107,116 @@ plot_no_cd4_nohisp + annotate("text", x = c(0.75, 1.2, 1.8, 2.2)
                               ,y = 10000, label = as.character(thecounts4$n)
                               ,size = 5)
 plot_no_cd4_hisp + annotate("text", x = c(0.75, 1.2, 1.8, 2.2, 2.8, 3.2)
+                            ,y = 10000, label = as.character(thecounts5$n)
+                            ,size = 5)
+dev.off()
+
+
+#exploring the relationship between income and frailty incidence in all colectomy patients
+#that have a Clavien-Dindo Grade 4 complication (TRUE) or not (FALSE)
+thedata <- dat1subs[["all_colon_all"]] %>% select(a_discrete_rai, a_any_cd4, income_final) %>% group_by( a_discrete_rai, a_any_cd4)
+ggplot(data = thedata, aes(x = factor(a_discrete_rai)
+                           ,y = income_final,fill=a_any_cd4)) + 
+  geom_violin(na.rm = TRUE) + labs(title = "Income Vs Frailty Vs Clavien-Dindo Grade 4 Complications in All UHS Colectomy Patients") +
+  scale_fill_discrete(name = "Clavien-Dindo Grade4"
+                      ,breaks = c("FALSE", "TRUE")
+                      ,labels = c("No", "Yes")
+  ) -> plot_any_cd4_violin;
+#' I should figure out how to print this table too:
+thecounts <- dat1subs[["all_colon_all"]] %>% select(a_discrete_rai, a_any_cd4, income_final) %>% group_by( a_discrete_rai, a_any_cd4) %>% count()
+plot_any_cd4_violin + annotate("text", x = c(0.8, 1.2, 1.8, 2.2, 2.8, 3.2)
+                        ,y = 10000, label = as.character(thecounts$n)
+                        ,size = 6)
+
+
+#selecting patients that DO NOT have Clavien-Dindo Grade 4 complications
+thedata2 <- dat1subs[["all_colon_all"]] %>% select(a_discrete_rai,hispanic_ethnicity, income_final, a_any_cd4) %>% group_by( a_discrete_rai,hispanic_ethnicity) %>% 
+  filter(a_any_cd4=='FALSE')
+ggplot(data = thedata2, aes(x = a_discrete_rai
+                            ,y = income_final, fill = hispanic_ethnicity)) + 
+  geom_violin() + labs(title = "Income Vs Frailty Vs Hispanic Ethnicity in All UHS Colectomy Patients with No CD4 Complications") +
+  scale_fill_discrete(name = "Ethnicity"
+                      ,breaks = c("No", "Unknown", "Yes")
+                      ,labels = c("Non-Hispanic", "Unknown", "Hispanic")
+  )-> plot_no_cd4_violin;
+
+#' I should figure out how to print this table too:
+thecounts2 <- dat1subs[["all_colon_all"]] %>% select(a_discrete_rai,hispanic_ethnicity, income_final, a_any_cd4) %>% group_by( a_discrete_rai,hispanic_ethnicity) %>% 
+  filter(a_any_cd4=='FALSE') %>% count()
+plot_no_cd4_violin + annotate("text", x = c(0.75, 1.0, 1.25, 1.8, 2.2, 3.0)
+                       ,y = 10000, label = as.character(thecounts2$n)
+                       ,size = 5)
+
+#selecting patients that HAVE Clavien-Dindo Grade 4 complications
+thedata3 <- dat1subs[["all_colon_all"]] %>% select(a_discrete_rai,hispanic_ethnicity, income_final, a_any_cd4) %>% group_by( a_discrete_rai,hispanic_ethnicity) %>% 
+  filter(a_any_cd4=='TRUE')
+ggplot(data = thedata3, aes(x = a_discrete_rai, y = income_final,fill=hispanic_ethnicity)) + 
+  geom_violin() + labs(title = "Income Vs Frailty Vs Hispanic Ethnicity in All UHS Colectomy Patients with CD4 Complications") +
+  scale_fill_discrete(name = "Ethnicity"
+                      ,breaks = c("No", "Unknown", "Yes")
+                      ,labels = c("Non-Hispanic", "Unknown", "Hispanic")
+  )-> plot_cd4_violin;
+#' I should figure out how to print this table too:
+thecounts3 <- dat1subs[["all_colon_all"]] %>% select(a_discrete_rai,hispanic_ethnicity, income_final, a_any_cd4) %>% group_by( a_discrete_rai,hispanic_ethnicity) %>% 
+  filter(a_any_cd4=='TRUE') %>% count()
+plot_cd4_violin + annotate("text", x = c(0.75, 1.0, 1.25, 1.8, 2.2, 3.0)
+                    ,y = 10000, label = as.character(thecounts3$n)
+                    ,size = 5)
+
+#selecting patients that DO NOT have Clavien-Dindo Grade 4 complications
+#and are not hispanic
+thedata4 <- dat1subs[["all_colon_all"]] %>% select(a_discrete_rai,hispanic_ethnicity, income_final, a_any_cd4) %>% group_by( a_discrete_rai) %>% 
+  filter(hispanic_ethnicity=='No')
+ggplot(data = thedata4, aes(x = a_discrete_rai
+                            ,y = income_final, fill = a_any_cd4)) + 
+  geom_violin() + 
+  labs(title = "Income Vs Frailty Vs CD4 Complications in Non-Hispanic UHS Colectomy Patients") +
+  scale_fill_discrete(name = "Clavien-Dindo Grade4"
+                      ,breaks = c("FALSE", "TRUE")
+                      ,labels = c("No", "Yes")
+  )-> plot_no_cd4_nohisp_violin;
+
+#' I should figure out how to print this table too:
+thecounts4 <- dat1subs[["all_colon_all"]] %>% select(a_discrete_rai,hispanic_ethnicity, income_final, a_any_cd4) %>% group_by( a_discrete_rai, a_any_cd4) %>% 
+  filter(hispanic_ethnicity=='No') %>% count()
+plot_no_cd4_nohisp_violin + annotate("text", x = c(0.75, 1.2, 1.8, 2.2)
+                              ,y = 10000, label = as.character(thecounts4$n)
+                              ,size = 5)
+#selecting patients that DO NOT have Clavien-Dindo Grade 4 complications
+#and are hispanic
+thedata5 <- dat1subs[["all_colon_all"]] %>% select(a_discrete_rai,hispanic_ethnicity, income_final, a_any_cd4) %>% group_by( a_discrete_rai) %>% 
+  filter(hispanic_ethnicity=='Yes')
+ggplot(data = thedata5, aes(x = a_discrete_rai
+                            ,y = income_final, fill = a_any_cd4)) + 
+  geom_violin() + 
+  labs(title = "Income Vs Frailty Vs CD4 Complications in UHS Hispanic Colectomy Patients") +
+  scale_fill_discrete(name = "Clavien-Dindo Grade4"
+                      ,breaks = c("FALSE", "TRUE")
+                      ,labels = c("No", "Yes")
+  )-> plot_no_cd4_hisp_violin;
+
+#' I should figure out how to print this table too:
+thecounts5 <- dat1subs[["all_colon_all"]] %>% select(a_discrete_rai,hispanic_ethnicity, income_final, a_any_cd4) %>% group_by(a_discrete_rai, a_any_cd4) %>% 
+  filter(hispanic_ethnicity=='Yes') %>% count()
+plot_no_cd4_hisp_violin + annotate("text", x = c(0.75, 1.2, 1.8, 2.2, 2.8, 3.2)
+                            ,y = 10000, label = as.character(thecounts5$n)
+                            ,size = 5)
+
+
+pdf(height = 10, width = 7.5, onefile = TRUE, file = paste0(outputpath,"UHS_ACSNSQIP_CD4comps_violinplots-DSW-", format(Sys.Date(), '%m-%d-%Y'),".pdf"))
+plot_any_cd4_violin + annotate("text", x = c(0.8, 1.2, 1.8, 2.2, 2.8, 3.2)
+                        ,y = 10000, label = as.character(thecounts$n)
+                        ,size = 6)
+plot_no_cd4_violin + annotate("text", x = c(0.75, 1.0, 1.25, 1.8, 2.2, 3.0)
+                       ,y = 10000, label = as.character(thecounts2$n)
+                       ,size = 5)
+plot_cd4_violin +annotate("text", x = c(0.75, 1.0, 1.25, 1.8, 2.2, 3.0)
+                   ,y = 10000, label = as.character(thecounts3$n)
+                   ,size = 5)
+plot_no_cd4_nohisp_violin + annotate("text", x = c(0.75, 1.2, 1.8, 2.2)
+                              ,y = 10000, label = as.character(thecounts4$n)
+                              ,size = 5)
+plot_no_cd4_hisp_violin + annotate("text", x = c(0.75, 1.2, 1.8, 2.2, 2.8, 3.2)
                             ,y = 10000, label = as.character(thecounts5$n)
                             ,size = 5)
 dev.off()
