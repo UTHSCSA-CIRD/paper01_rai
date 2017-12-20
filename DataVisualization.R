fb99686e3568e4e31dc5286678a131ff433a25cb
Desiree Wilson <wilsond3@uthscsa.edu>
2017-12-20 09:40:29 -0600

    Updated the 'DataVisualization.R' file to include the relationship between CD4, Hispanic ethnicity and median income.

--- DataVisualization.R
+++ DataVisualization.R
@@ -92,6 +92,35 @@ plot_no_cd4_hisp + annotate("text", x = c(0.75, 1.2, 1.8, 2.2, 2.8, 3.2)
                             ,y = 10000, label = as.character(thecounts5$n)
                             ,size = 5)
 
+#selecting patients that DO NOT have Clavien-Dindo Grade 4 complications
+#and are hispanic
+d2 <- rbind(dat1subs[["all_colon_all"]], dat1subs[["all_colon_all"]])
+d2$hispanic_ethnicity2 <- c(rep("All", times=nrow(dat1subs[["all_colon_all"]])), dat1subs[["all_colon_all"]]$hispanic_ethnicity)
+#re-leveling ethnicity
+d2$hispanic_ethnicity3 <- factor(d2$hispanic_ethnicity2, levels = c("All", "No", "Yes", "Unknown"))
+
+
+thedata6 <- d2 %>% select(hispanic_ethnicity3, income_final, a_any_cd4) %>% 
+  filter(hispanic_ethnicity3 %in% c("All", "No", "Yes")) %>% group_by( hispanic_ethnicity3, a_any_cd4) 
+
+ggplot(data = thedata6, aes(x = hispanic_ethnicity3
+                            ,y = income_final, fill = a_any_cd4)) + 
+  geom_boxplot(coef=100) + 
+  labs(title = "Income Vs CD4 Complications in UHS Hispanic Colectomy Patients") +
+  scale_y_continuous(labels = comma) +
+  scale_fill_discrete(name = "Clavien-Dindo Grade4"
+                      ,breaks = c("FALSE", "TRUE")
+                      ,labels = c("No", "Yes")) +
+  scale_x_discrete(name = "Ethnicity"
+                   ,breaks = c("All", "No", "Yes")
+                   ,labels = c("All", "Non-Hispanic", "Hispanic")
+  ) -> plot_no_rai_cd4_hisp;
+
+#' I should figure out how to print this table too:
+thecounts6 <- thedata6 %>% count()
+plot_no_rai_cd4_hisp + annotate("text", x = c(0.75, 1.2, 1.8, 2.25, 2.8, 3.2)
+                                       ,y = 5000, label = as.character(thecounts5$n)
+                                       ,size = 5)
 
 pdf(height = 10, width = 7.5, onefile = TRUE, file = paste0(outputpath,"UHS_ACSNSQIP_CD4comps_boxplots-DSW-", format(Sys.Date(), '%m-%d-%Y'),".pdf"))
 plot_any_cd4 + annotate("text", x = c(0.8, 1.2, 1.8, 2.2, 2.8, 3.2)
@@ -201,6 +230,38 @@ thecounts5 <- dat1subs[["all_colon_all"]] %>% select(a_discrete_rai,hispanic_eth
 plot_no_cd4_hisp_violin + annotate("text", x = c(0.75, 1.2, 1.8, 2.2, 2.8, 3.2)
                             ,y = 10000, label = as.character(thecounts5$n)
                             ,size = 5)
+
+
+#selecting patients that DO NOT have Clavien-Dindo Grade 4 complications
+#and are hispanic
+d2 <- rbind(dat1subs[["all_colon_all"]], dat1subs[["all_colon_all"]])
+d2$hispanic_ethnicity2 <- c(rep("All", times=nrow(dat1subs[["all_colon_all"]])), dat1subs[["all_colon_all"]]$hispanic_ethnicity)
+#re-leveling ethnicity
+d2$hispanic_ethnicity3 <- factor(d2$hispanic_ethnicity2, levels = c("All", "No", "Yes", "Unknown"))
+
+
+thedata6 <- d2 %>% select(hispanic_ethnicity3, income_final, a_any_cd4) %>% 
+  filter(hispanic_ethnicity3 %in% c("All", "No", "Yes")) %>% group_by( hispanic_ethnicity3, a_any_cd4) 
+
+  ggplot(data = thedata6, aes(x = hispanic_ethnicity3
+                            ,y = income_final, fill = a_any_cd4)) + 
+  geom_violin(coef=100) + 
+  labs(title = "Income Vs CD4 Complications in UHS Hispanic Colectomy Patients") +
+  scale_y_continuous(labels = comma) +
+  scale_fill_discrete(name = "Clavien-Dindo Grade4"
+                      ,breaks = c("FALSE", "TRUE")
+                      ,labels = c("No", "Yes")) +
+  scale_x_discrete(name = "Ethnicity"
+                   ,breaks = c("All", "No", "Yes")
+                   ,labels = c("All", "Non-Hispanic", "Hispanic")
+                   ) -> plot_no_rai_cd4_hisp_violin;
+
+#' I should figure out how to print this table too:
+thecounts6 <- thedata6 %>% count()
+plot_no_rai_cd4_hisp_violin + annotate("text", x = c(0.75, 1.2, 1.8, 2.25, 2.8, 3.2)
+                                   ,y = 5000, label = as.character(thecounts5$n)
+                                   ,size = 5)
+
 
 
 pdf(height = 10, width = 7.5, onefile = TRUE, file = paste0(outputpath,"UHS_ACSNSQIP_CD4comps_violinplots-DSW-", format(Sys.Date(), '%m-%d-%Y'),".pdf"))
