41855988b7dec51665a709b814978adec2264a15
Desiree Wilson <wilsond3@uthscsa.edu>
2018-01-10 11:48:43 -0600

    I created a summary table specifically for 2016 colectomy data.

--- SummaryTables.R
+++ SummaryTables.R
@@ -77,3 +77,60 @@ tables_02 <- lapply(tables_01, function(xx) {
 #' Here', I'm writing the tables to a file:
 lapply(tables_02, write.table, paste0(outputpath, 'UHS_ACSNSQIP_SummaryTables-DSW-', format(Sys.Date(), '%m-%d-%Y'),'.xlsx'), row.names=FALSE, append=TRUE, sep='\t')
 
+#Focus on UHS colectomy data January 10, 2018:
+table_01 <- dat1subs[["all_colon_all"]] %>% 
+  filter(hospital_admissn_dt > "2015-12-31" & hospital_admissn_dt < "2017-01-01") %>% 
+  group_by(rai_range) %>% 
+  summarize(rai_n = n()	    
+            ,cumul_count = cumsum(n())
+            ,died_n = sum(postop_death_30_dy_proc =='Yes') 
+            ,died_frac = mean(postop_death_30_dy_proc =='Yes')
+            ,comp_n = sum(a_any_postop=='TRUE')
+            ,comp_frac = mean(a_any_postop=='TRUE')
+            ,cd4_n = sum(a_any_cd4=='TRUE')
+            ,cd4_frac = mean(a_any_cd4=='TRUE')
+            ,readmsn_n = sum(a_readm_30_dy=='TRUE')
+            ,readmsn_frac = mean(a_readm_30_dy=='TRUE')
+  ) %>% 
+  mutate(cumul_count=rev(cumsum(rev(rai_n)))) %>% 
+  arrange(desc(rai_range)) 
+renamecol <- function(xx) {
+  rai_range <- "Total"
+  rai_n <- sum(xx$rai_n)
+  cumul_count <- xx$cumul_count[nrow(xx)]
+  died_n <- sum(xx$died_n)
+  #this is how Dr. Shireman wanted the
+  #summary fraction reported:
+  #the total number of people in cumulative
+  #count in each table divided by the
+  #total number of people that died (in this case)
+  died_frac <- sum(xx$died_n)/cumul_count
+  comp_n <- sum(xx$comp_n)
+  #the total number of people in cumulative
+  #count in each table divided by the
+  #total number of people that had
+  #complications (in this case)
+  comp_frac <- sum(xx$comp_n)/cumul_count
+  cd4_n <- sum(xx$cd4_n)
+  #the total number of people in cumulative
+  #count in each table divided by the
+  #total number of people that had
+  #Clavien-Dindo Grade 4 complications (in this case)
+  cd4_frac <- sum(xx$cd4_n)/cumul_count
+  readmsn_n <- sum(xx$readmsn_n)
+  #the total number of people in cumulative
+  #count in each table divided by the
+  #total number of people that had
+  #readmissions (in this case)
+  readmsn_frac <- sum(xx$readmsn_n)/cumul_count
+  newtotals <- t(c(rai_range, rai_n, cumul_count
+                   ,died_n, died_frac, comp_n
+                   ,comp_frac, cd4_n, cd4_frac
+                   ,readmsn_n, readmsn_frac))
+  colnames(newtotals) <- colnames(xx)
+  newxx <- rbind(xx, newtotals)
+  colnames(newxx) <- thecolnames
+  return(newxx)
+}
+
+table_02 <- renamecol(table_01)
