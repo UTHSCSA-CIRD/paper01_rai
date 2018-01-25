5b0dffd6e5780d93472da0e9187413a29fb1a79d
Alex F. Bokov <bokov@uthscsa.edu>
2018-01-25 11:05:52 -0500

    Temporary workaround test

--- DataVisualization.R
+++ DataVisualization.R
@@ -34,7 +34,7 @@ plt_eth_inc_cd4 <- autoboxplot(sbs0$all$all_colon_all
                                ,xx='hispanic_ethnicity',yy='income_final'
                                ,zz='a_any_cd4'
                                ,subset=hispanic_ethnicity!='Unknown'&!is.na(income_final)
-                               ,fill.name=wrap_format(14)('Clavien-Dindo Grade 4')
+                               ,fill.name=titlehack
                                ,fill.labels = c('Yes', 'No')
                                ,xx.name='Hispanic Ethnicity',yy.name=NA,title='');
 
