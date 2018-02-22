14ab2c747555856ef86d1e566f035691c17d4b59
Alex F. Bokov <bokov@uthscsa.edu>
2018-02-22 11:49:26 -0500

    Replaced a bunch of code with shorter countfrac() expressions, removed duplicate survival plots, added a TableOne at the end for the demographic summary

--- ForPoster.R
+++ ForPoster.R
@@ -10,7 +10,7 @@ knitr::opts_chunk$set(echo=F,warning = F,cache=T,message=F);
 source('global.R');
 #' Report date: `r date()`.
 #'
-#' Revision: `r gitstamp()`.
+# Revision: `r gitstamp()`.
 #'
 #' Data file: `r inputdata`.
 #' 
@@ -19,15 +19,14 @@ source('global.R');
 #' 
 #' 
 #+ cache=TRUE
-
 source('run.R');
 surv.rai <- survfit(Surv(a_t,a_c) ~ I(a_rai>median(a_rai))
                     , data = sbs0$all$all_emergency,subset=a_t>0);
 surv.rock <- survfit(Surv(a_t,a_c) ~ I(a_rockwood>median(a_rockwood))
                      , data = sbs0$all$all_emergency,subset=a_t>0);
 
-ggsurvplot(surv.rai);
-ggsurvplot(surv.rock);
+#ggsurvplot(surv.rai);
+#ggsurvplot(surv.rock);
 
 
 pl_surv <-list(RAI=autoplot(surv.rai), Rockwood=autoplot(surv.rock));
@@ -40,48 +39,41 @@ pl_surv <- sapply(names(pl_surv)
                     labs(x='Time in Days', y = 'Survival'),simplify=F);
 multiplot(plotlist=pl_surv,cols=1);
 
-sbs0$all$all_emergency$a_rockwood_range <- cut(sbs0$all$all_emergency$a_rockwood, 7)
-rai_table <- sbs0$all$all_emergency %>%  group_by(rai_range) %>% 
-         summarise(rai_n = n()	    
-                   ,cumul_count = cumsum(n())
-                   ,died_n = sum(postop_death_30_dy_proc =='Yes') 
-                   ,died_frac = mean(postop_death_30_dy_proc =='Yes')
-                   ,readmsn_n = sum(a_readm_30_dy=='TRUE')
-                   ,readmsn_frac = mean(a_readm_30_dy=='TRUE')
-         ) %>% 
-         mutate(cumul_count=rev(cumsum(rev(rai_n)))) %>% 
-         arrange(desc(rai_range));
+# This is the creation of an analytic variable. Therefore it should happen in run.R
+# Also, this should be done on dat1, right after a_rockwood gets created, so all
+# the other subsets can inherit it. I moved it there.
+#sbs0$all$all_emergency$a_rockwood_range <- cut(sbs0$all$all_emergency$a_rockwood, 7)
 
-thecolnames <- c("RAI-A Range"
-                 ,"RAI-A Score N"
-                 ,"Cumulative Count"
-                 ,"Died 30days N"
-                 ,"Died 30days Fraction"
-                 ,"30day Readmission N"
-                 ,"30day Readmission Fraction");
+thecolnames1 <- c("RAI-A Score"='rai_range'
+                  ,"Rockwood Score"='a_rockwood_range'
+                  ,"Income"='income_final'
+                  ,"Gender"='gender'
+                  ,"Hispanic"='hispanic_ethnicity'
+                  ,"BMI"='bmi'
+                  ,"Age"='age_at_time_surg'
+                  ,"Count"='bin_n'
+                  ,"Cumulative Count"='cumul_count'
+                  ,"Died 30d"="postop_death_30_dy_proc_n"
+                  ,"Died 30d Frac."="postop_death_30_dy_proc_frac"
+                  ,"Readmission 30d"="a_readm_30_dy_n"
+                  ,"Readmission 30d Frac."="a_readm_30_dy_frac"
+);
 
-colnames(rai_table) <- thecolnames;
-kable(rai_table)
+# We set the default value of the outcomes argument for the purposes of this 
+# script so we don't have to keep repeating it.
+formals(countfrac)$outcomes <- c('postop_death_30_dy_proc','a_readm_30_dy');
+# Now we render the tables. You don't need all that stuff below.
+countfrac(sbs0$all$all_emergency,groupcols = 'a_rockwood_range') %>% 
+  mapnames(thecolnames1) %>% kable(format='markdown',digits = 2);
+countfrac(sbs0$all$all_emergency,groupcols = 'rai_range') %>% 
+  mapnames(thecolnames1) %>% kable(format='markdown',digits = 2);
 
-
-rock_table <- sbs0$all$all_emergency %>%  group_by(a_rockwood_range) %>% 
-  summarise(rai_n = n()	    
-            ,cumul_count = cumsum(n())
-            ,died_n = sum(postop_death_30_dy_proc =='Yes') 
-            ,died_frac = mean(postop_death_30_dy_proc =='Yes')
-            ,readmsn_n = sum(a_readm_30_dy=='TRUE')
-            ,readmsn_frac = mean(a_readm_30_dy=='TRUE')
-  ) %>% 
-  mutate(cumul_count=rev(cumsum(rev(rai_n)))) %>% 
-  arrange(desc(a_rockwood_range));
-
-thecolnames2 <- c("Rockwood Range"
-                 ,"Rockwood Score N"
-                 ,"Cumulative Count"
-                 ,"Died 30days N"
-                 ,"Died 30days Fraction"
-                 ,"30day Readmission N"
-                 ,"30day Readmission Fraction");
-
-colnames(rock_table) <- thecolnames2;
-kable(rock_table)
+#' ### Demographics
+#+ results='asis'
+mutate(sbs0$all$all_emergency,`RAI-A`=factor(a_rai>median(a_rai)
+                                             ,levels = c('FALSE','TRUE')
+                                             ,labels=c('Low','High'))) %>% 
+  mapnames(thecolnames1) %>% 
+  CreateTableOne(names(thecolnames1)[3:7],'RAI-A',.) %>% 
+  print(printToggle=F,noSpaces=T) %>% `[`(,-4) %>% 
+  kable(format='markdown');
