3d5e6e09fe2b32caba6f166d20c23878d58144b1
Alex F. Bokov <bokov@uthscsa.edu>
2018-02-23 02:24:46 -0500

    Reflowed comments, fixed resulting problem with bullet points, fixed TableOne, (successfully) experimented with output options in the YAML header, split up tables that were running together in word, replaced HTML image tags with the universally compatible markdown-producing function include_graphics(). But we'll still need to change the chunk options where it is used to avoid giant pictures.

--- ForPoster.R
+++ ForPoster.R
@@ -2,10 +2,17 @@
 #' title: "RAI Main Analysis"
 #' author: "Wilson, Bokov, Shireman"
 #' date: "08/15/2017"
+#' output: 
+#'   word_document:
+#'     keep_md: true
+#'     reference_docx: styletemplate0.docx
 #' bibliography: Frailty.bib
 #' ---
 #'
 #+ echo=FALSE, message=FALSE
+# output params backup
+#   html_document:
+#     keep_md: true
 knitr::opts_chunk$set(echo=F,warning = F,cache=F,message=F);
 options(knitr.kable.NA='');
 #+ cache=FALSE
@@ -42,7 +49,7 @@ thecolnames1 <- c("RAI-A Score"='rai_range'
                   ,"N"='n'
                   ,"Events"='nevent'
                   ,"Effect"='estimate'
-                  ,"Std. Err."='std.error'
+                 ,"Std. Err."='std.error'
                   ,"Estimate/SE"='statistic'
                   ,"P"='p.value'
                   ,"R^2"='r.squared'
@@ -64,36 +71,99 @@ formals(countfrac)$outcomes <- c('postop_death_30_dy_proc','a_readm_30_dy');
 #' 
 #' # Introduction
 #' 
-#' * RAI-A good for predicting surgical risk [@HallDevelopmentInitialValidation2017]
-#' * ...but it cannot be used on live data because it's hard-coded to NSQIP
-#' * The availability of NSQIP variables can differ between sites with different
-#'   levels of participation and some variables may be discontinued.
-#' * Rockwood Index can be calculated on anything, tolerates missing data,
-#'   and therefore it can be calculated not only on manually curated registry
-#'   data but on any other sources of laboratory values and diagnoses including
-#'   billing records and EMR systems.
-#' * Rockwood has been thoroughly vetted by geriatricians and its statistical
-#'   assumptions have been explicitly tested.
-#' * Rockwood could be used for automated risk-calculation to support surgical
-#'   decisions it it could be shown that it is as good as or better than RAI-A
+#' * RAI-A good for predicting surgical risk [@HallDevelopmentInitialValidation2017] 
+#' * ...but it cannot be used on live data because it's hard-coded to NSQIP 
+#' * The availability of NSQIP variables can differ between sites with different 
+#'   levels of participation and some variables may be discontinued. 
+#' * Rockwood Index can be calculated on anything, tolerates missing data, and 
+#'   therefore it can be calculated not only on manually curated registry data 
+#'   but on any other sources of laboratory values and diagnoses including 
+#'   billing records and EMR systems
+#' * Rockwood has been thoroughly vetted by geriatricians and its statistical 
+#'   assumptions have been explicitly tested. 
+#' * Rockwood could be used for automated risk-calculation to support surgical 
+#'   decisions it it could be shown that it is as good as or better than RAI-A 
 #'   for predicting surgical outcomes.
-#'   
-#'   Frailty is the inability to maintain homeostasis when the human body 
-#'   becomes challenged by a daily stressor [Torpy JM, Lymn C and Glass RM. Frailty in Older Adults. JAMA. 2006;296(18):2280. doi:10.1001/jama.296.18.2280].  Low physical activity, muscle weakness, slowed performance, fatigue and unintentional weight loss are all characteristics of frailty [Torpy JM, Lymn C and Glass RM. Frailty in Older Adults. JAMA. 2006;296(18):2280. doi:10.1001/jama.296.18.2280]. As the U.S. population ages, frailty will be a growing concern for the US. According to the U.S. Census Bureau Population Division, the U.S. population aged 65 and older is projected to increase by 45% (from 55 million to 80 million) by 2050 [U.S. Census Bureau, Population Division. 2012. “Table 12. Projections of the Population by Age and Sex for the United States: 2015 to 2060 (NP2012-T12)”]. This population group alone will make up approximately 20% of the U.S. population in 2050 [U.S. Census Bureau, Population Division. 2012. “Table 12. Projections of the Population by Age and Sex for the United States: 2015 to 2060 (NP2012-T12)”]. While it is true that the 45 to 64 age group has a higher percentage of ambulatory surgeries compared to the 65 and above age demographic, the risk of adverse surgical outcomes is higher in the latter compared to the former [Hall MJ, Schwartzman A, Zhang J and Liu X. Ambulatory Surgery Data From Hospitals and Ambulatory Surgery Centers: United States, 2010. Natl Health Stat Report. 2017 Feb;(102):1-15;  Polanczyk CA, Marcantonio E, Goldman L, et al. Impact of age on perioperative complications and length of stay in patients undergoing noncardiac surgery. Ann Intern Med. 2001;134(8):637- 643. doi:10.7326/0003-4819-134-8-200104170-00008]. Furthermore, the number of overnight hospital stays increases with age [Lucas JW, Benson V. Tables of Summary Health Statistics for the U.S. Population: 2015 National Health Interview Survey. National Center for Health Statistics. 2017. Available from:  http://www.cdc.gov/nchs/nhis/SHS/tables.htm ]. Frailty is associated with increased risk of post-operative complications and does increase with age [Brahmbhatt R, Brewster LP, Shafii S, et al. Gender and frailty predict poor outcomes in infrainguinal vascular surgery. J Surg Res. 201(1), 156-165. DOI: 10.1016/j.jss.2015.10.026]. Based on these findings, there seems to be an overlap between the frail population and the high medical need population (i.e. individuals that repeatedly visit the hospitals for serious health concerns). Therefore, frailty could be one way to identify a subpopulation of high-need patients prior to 30 day post-operative readmission. 
-#'
-#' There are many frailty metrics that were developed to identify frail patients. Generally, these metrics fall in two major categories: metrics that require a physical interaction with the patient (such as a physical assessment or completion of a questionnaire); and metrics obtained from a patient’s electronic health record (i.e. no patient contact). Inside an electronic health record (EHR) lies a wealth of billing, medical and sociodemographic information that could be used to 1) classify patients based on a frailty score and 2) predict the likelihood of a post-operative complication experienced by the patient. Since our strength is with EHR analysis, this study chose to focus on two frailty metrics that lend themselves well to EHR data: the Risk Analysis Index (RAI) and the Rockwood & Mitnitsky Frailty Index [Hall DE, Arya S, Schmid KK, et al. Development and Initial Validation of the Risk Analysis Index for Measuring Frailty in Surgical Populations. JAMA Surg. 2017 Feb 1;152(2):175-182. doi: 10.1001/jamasurg.2016.4202;  Mitnitski AB, Mogilner AJ and Rockwood K. Accumulation of deficits as a proxy measure of aging. Scientific World Journal. 2001 Aug 8;1:323-36]. The RAI-A (RAI Administrative) frailty metric is a frailty screening tool 
-#'
-#'   
+#' 
+#' Frailty is the inability to maintain homeostasis when the human body becomes
+#' challenged by a daily stressor [Torpy JM, Lymn C and Glass RM. Frailty in
+#' Older Adults. JAMA. 2006;296(18):2280. doi:10.1001/jama.296.18.2280].  Low
+#' physical activity, muscle weakness, slowed performance, fatigue and
+#' unintentional weight loss are all characteristics of frailty [Torpy JM, Lymn
+#' C and Glass RM. Frailty in Older Adults. JAMA. 2006;296(18):2280.
+#' doi:10.1001/jama.296.18.2280]. As the U.S. population ages, frailty will be a
+#' growing concern for the US. According to the U.S. Census Bureau Population
+#' Division, the U.S. population aged 65 and older is projected to increase by
+#' 45% (from 55 million to 80 million) by 2050 [U.S. Census Bureau, Population
+#' Division. 2012. “Table 12. Projections of the Population by Age and Sex for
+#' the United States: 2015 to 2060 (NP2012-T12)”]. This population group alone
+#' will make up approximately 20% of the U.S. population in 2050 [U.S. Census
+#' Bureau, Population Division. 2012. “Table 12. Projections of the Population
+#' by Age and Sex for the United States: 2015 to 2060 (NP2012-T12)”]. While it
+#' is true that the 45 to 64 age group has a higher percentage of ambulatory
+#' surgeries compared to the 65 and above age demographic, the risk of adverse
+#' surgical outcomes is higher in the latter compared to the former [Hall MJ,
+#' Schwartzman A, Zhang J and Liu X. Ambulatory Surgery Data From Hospitals and
+#' Ambulatory Surgery Centers: United States, 2010. Natl Health Stat Report.
+#' 2017 Feb;(102):1-15;  Polanczyk CA, Marcantonio E, Goldman L, et al. Impact
+#' of age on perioperative complications and length of stay in patients
+#' undergoing noncardiac surgery. Ann Intern Med. 2001;134(8):637- 643.
+#' doi:10.7326/0003-4819-134-8-200104170-00008]. Furthermore, the number of
+#' overnight hospital stays increases with age [Lucas JW, Benson V. Tables of
+#' Summary Health Statistics for the U.S. Population: 2015 National Health
+#' Interview Survey. National Center for Health Statistics. 2017. Available
+#' from:  http://www.cdc.gov/nchs/nhis/SHS/tables.htm ]. Frailty is associated
+#' with increased risk of post-operative complications and does increase with
+#' age [Brahmbhatt R, Brewster LP, Shafii S, et al. Gender and frailty predict
+#' poor outcomes in infrainguinal vascular surgery. J Surg Res. 201(1), 156-165.
+#' DOI: 10.1016/j.jss.2015.10.026]. Based on these findings, there seems to be
+#' an overlap between the frail population and the high medical need population
+#' (i.e. individuals that repeatedly visit the hospitals for serious health
+#' concerns). Therefore, frailty could be one way to identify a subpopulation of
+#' high-need patients prior to 30 day post-operative readmission.
+#' 
+#' There are many frailty metrics that were developed to identify frail
+#' patients. Generally, these metrics fall in two major categories: metrics that
+#' require a physical interaction with the patient (such as a physical
+#' assessment or completion of a questionnaire); and metrics obtained from a
+#' patient’s electronic health record (i.e. no patient contact). Inside an
+#' electronic health record (EHR) lies a wealth of billing, medical and
+#' sociodemographic information that could be used to 1) classify patients based
+#' on a frailty score and 2) predict the likelihood of a post-operative
+#' complication experienced by the patient. Since our strength is with EHR
+#' analysis, this study chose to focus on two frailty metrics that lend
+#' themselves well to EHR data: the Risk Analysis Index (RAI) and the Rockwood &
+#' Mitnitsky Frailty Index [Hall DE, Arya S, Schmid KK, et al. Development and
+#' Initial Validation of the Risk Analysis Index for Measuring Frailty in
+#' Surgical Populations. JAMA Surg. 2017 Feb 1;152(2):175-182. doi:
+#' 10.1001/jamasurg.2016.4202;  Mitnitski AB, Mogilner AJ and Rockwood K.
+#' Accumulation of deficits as a proxy measure of aging. Scientific World
+#' Journal. 2001 Aug 8;1:323-36]. The RAI-A (RAI Administrative) frailty metric
+#' is a frailty screening tool
+#' 
+#' 
 #' # Methods
 #' 
-#' A total of 6408 University Hospital System (UHS) cases were used in this study. These cases occurred between April 2013 to February 2017. A total of 140 annotated and highly curated variables were extracted from each EHR, among them was address. In order to determine a median income approximation for each case, we linked the census block group level data from the 2015 American Community Survey Median Income table to each cases’ address. To calculate the the RAI-A frailty score for each case, we used the calculation described in Hall DE, Arya S, Schmid KK, et al. Development and Initial Validation of the Risk Analysis Index for Measuring Frailty in Surgical Populations. JAMA Surg. 2017 Feb 1;152(2):175-182. doi: 10.1001/jamasurg.2016.4202. In short, the calculation can be seen here:
-#' <div id="rai">
-#'   <img src="RAI-Cv2.png" alt="">
-#' </div> 
-#' Additionally, the Rockwood index was calculated for each case using the following paper: Mitnitski AB, Mogilner AJ and Rockwood K. Accumulation of deficits as a proxy measure of aging. Scientific World Journal. 2001 Aug 8;1:323-36. Briefly, the Rockwood index for each patient is calculated by the following equation: 
-#' <div id="rai">
-#'   <img src="FormulaV2.png" alt="">
-#' </div>
+#' A total of 6408 University Hospital System (UHS) cases were used in this 
+#' study. These cases occurred between April 2013 to February 2017. A total of 
+#' 140 annotated and highly curated variables were extracted from each EHR, 
+#' among them was address. In order to determine a median income approximation
+#' for each case, we linked the census block group level data from the 2015
+#' American Community Survey Median Income table to each cases’ address. To
+#' calculate the the RAI-A frailty score for each case, we used the calculation
+#' described in Hall DE, Arya S, Schmid KK, et al. Development and Initial
+#' Validation of the Risk Analysis Index for Measuring Frailty in Surgical
+#' Populations. JAMA Surg. 2017 Feb 1;152(2):175-182. doi:
+#' 10.1001/jamasurg.2016.4202. In short, the calculation can be seen here:
+# Using the include_graphics() command rather than HTML allows this to render 
+# properly in Word (and hopefully PDF?)
+include_graphics('RAI-Cv2.png');
+#' Additionally, the Rockwood index was calculated for each case using the
+#' following paper: Mitnitski AB, Mogilner AJ and Rockwood K. Accumulation of
+#' deficits as a proxy measure of aging. Scientific World Journal. 2001 Aug
+#' 8;1:323-36. Briefly, the Rockwood index for each patient is calculated by the
+#' following equation:
+include_graphics('FormulaV2.png');
 #' 
 #' ## Data Sources
 #' 
@@ -104,12 +174,12 @@ formals(countfrac)$outcomes <- c('postop_death_30_dy_proc','a_readm_30_dy');
 #' 
 #' ## Patient Demographics
 #+ table_demog,results='asis'
-mutate(sbs0$all$all_emergency,a_rai=factor(a_rai>median(a_rai)
+mutate(sbs0$all$all_emergency,t_strata=factor(a_c==1
                                              ,levels = c('FALSE','TRUE')
-                                             ,labels=c('Low','High'))) %>% 
+                                             ,labels=c('No Event','Death/Readmission'))) %>% 
   mapnames(thecolnames1) %>% 
-  CreateTableOne(names(thecolnames1)[5:9],'RAI-A',.) %>% 
-  print(printToggle=F,noSpaces=T) %>% `[`(-4) %>% 
+  CreateTableOne(names(thecolnames1)[3:9],'t_strata',.) %>% 
+  print(printToggle=F,noSpaces=T) %>% `[`(,-4) %>% 
   kable(format='markdown');
 
 #' ## Analysis
@@ -157,6 +227,7 @@ t_coxresults <- sapply(fit_coxs<-list(`RAI-A`=cox.rai.train,`Rockwood`=cox.rock.
                                 ,'p.value','r.squared','concordance'
                                 ,'std.error.concordance','AIC','BIC'));
 mapnames(t_coxresults,thecolnames1) %>% t %>% kable(format = 'markdown',digits=4);
+cat('\n---\n');
 #starkable(cox.rai.train,-1,searchrep = cbind(c('V1','Dependent variable','a_rai|a_rockwood'),c('Statistic','Value','Effect (SD)')));
 #starkable(cox.rock.train,-1,searchrep = cbind(c('V1','Dependent variable','a_rai|a_rockwood'),c('Statistic','Value','Effect (SD)')));
 # .junk<-capture.output(stargazer(cox.rai.train,type='html',omit.table.layout = 'n-!=!d'
@@ -173,12 +244,14 @@ mapnames(t_coxresults,thecolnames1) %>% t %>% kable(format = 'markdown',digits=4
 #+ table_counts, results='asis'
 countfrac(sbs0$all$all_emergency,groupcols = 'a_rockwood_range') %>% 
   mapnames(thecolnames1) %>% kable(format='markdown',digits = 2);
+cat('\n---\n');
 countfrac(sbs0$all$all_emergency,groupcols = 'rai_range') %>% 
   mapnames(thecolnames1) %>% kable(format='markdown',digits = 2);
 #' 
 #' ## RAI-A and Rockwood have equivalent concordances and AUCs
 #' 
-#' As can be seen from table 2, the concordances are `r do.call(sprintf,c('%0.2f (SE=%0.2f)',t_coxresults['RAI-A',c('concordance','std.error.concordance')]))`
+#' As can be seen from table 2, the concordances are 
+#' `r do.call(sprintf,c('%0.2f (SE=%0.2f)',t_coxresults['RAI-A',c('concordance','std.error.concordance')]))`
 #' and `r do.call(sprintf,c('%0.2f (SE=%0.2f)',t_coxresults['Rockwood',c('concordance','std.error.concordance')]))` for
 #' the Cox models whose predictors are RAI-A and Rockwood, respectively. Their 
 #' Receiver-Operator Characteristic (ROC) curves can be seen in Figure 2, along 
@@ -198,15 +271,14 @@ legend('topleft',bty ='n',col=c('orange','darkgreen'),lwd=3
 #' the threshold value that maximized sensitivity and specificity.
 #' 
 #' #### Optimal threshold scores for RAI-A and Rockwood
-#+ results='asis'
+#+ tab_snsp, results='asis'
 lapply(l_rocs,coords,'b',ret=c('threshold','sensitivity','specificity','accuracy'
-                               #,'tn','tp','fn','fp'
                                ,'npv','ppv','precision','recall')) %>% 
   lapply(mapnames,thecolnames1) %>% 
   lapply(function(xx) setNames(xx,capitalize(names(xx)))) %>% data.frame %>% 
   mapnames(thecolnames1) %>% kable(format='markdown',digits=3);
 #' 
-#+ results='asis'
+#+ tab_errormtx, results='asis'
 lapply(l_rocs,coords,'b',ret=c('tn','fn','fp','tp')) %>% 
   lapply(mapnames,thecolnames1) %>% 
   lapply(function(xx) paste(names(xx),'=',xx)) %>% 
@@ -215,6 +287,9 @@ lapply(l_rocs,coords,'b',ret=c('tn','fn','fp','tp')) %>%
   lapply(kable,format='markdown') %>% mapnames(thecolnames1) %>% 
   setNames(.,paste0(names(.),' Error Matrix')) %>% capture.output() %>% 
   gsub('^[$`]{1,2}','\n\n#### ',.) %>% gsub('`','',.) %>% cat(sep='\n');
+# TODO
+# * survROC cross-validation
+# * change TableOne-- all, survivors, and non-survivors
 #' # Discussion and Conclusions
 #' 
 #' # Acknowledgments
