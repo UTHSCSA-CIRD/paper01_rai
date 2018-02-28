f9b51f4e0876420c8966f3ba6fef303f19f7bdba
Alex F. Bokov <bokov@uthscsa.edu>
2018-02-28 17:54:15 -0600

    Updating global.R from main branch

--- global.R
+++ global.R
@@ -10,11 +10,14 @@ rq_libs <- c('compiler'                                   # just-in-time compila
              ,'MatchIt','DHARMa'                          # propensity scores and glm residuals
              ,'pscl'                                      # zero-inflated poisson, sigh
              ,'survival','MASS','Hmisc','zoo','coin'      # various analysis methods
+             ,'survAUC','survivalROC','pROC'
              ,'Matrix'                                    # for pd matrices needed by faker()
              ,'readr','dplyr','stringr','magrittr'        # data manipulation & piping
+             ,'lubridate'
              ,'ggplot2','ggfortify','grid','GGally'       # plotting
-             ,'gridExtra','scales'
-             ,'stargazer','broom', 'tableone','janitor'); # table formatting
+             ,'survminer','gridExtra','scales'
+             ,'stargazer','broom', 'tableone','janitor'   # table formatting
+             ,'knitr','htmltab');
 rq_installed <- sapply(rq_libs,require,character.only=T);
 rq_need <- names(rq_installed[!rq_installed]);
 if(length(rq_need)>0) install.packages(rq_need,repos='https://cran.rstudio.com/',dependencies = T);
