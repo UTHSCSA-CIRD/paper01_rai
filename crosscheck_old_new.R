183d124681a398607c06e6779a974544a78bd8b0
Alex F. Bokov <bokov@uthscsa.edu>
2018-06-24 12:56:39 -0500

    New branch, for comparing results generated from old data to results generated from new

---
+++ crosscheck_old_new.R
@@ -0,0 +1,48 @@
+#' ---
+#' title: "RAI Data Crosschecks"
+#' author: "Bokov, Shireman"
+#' date: "06/24/2018"
+#' ---
+#'
+#+ echo=FALSE, message=FALSE
+knitr::opts_chunk$set(echo=F,warning = F,cache=T,message=F);
+#+ cache=FALSE
+source('global.R');
+#' 
+#' Report date: `r Sys.Date()`.
+#'
+#' Revision: `r gitstamp()`.
+#'
+#' The purpose of this file is to load two different R environments created by
+#' the same version of `run.R` on two different input files and compare them. To
+#' enable this, assign valid paths to `inputenv_old` and `inputenv_new` in your
+#' `config.R` file. Otherwise, it will default to comparing identical copies of
+#' the current `inputdata` environment created using the current `run.R` file.
+#+ cache=TRUE
+if(!(exists('inputenv_new')&&file.exists(inputenv_new))){
+    # warn
+    if(!file.exists(session)){
+      system(sprintf('R -e "source(\'run.R\'); save.image(file=\'%s\')"',session));
+    }
+  inputenv_new <- session;
+}
+if(!(exists('inputenv_old')&&file.exists(inputenv_old))){
+  # warn
+  inputenv_old <- inputenv_new;
+}
+#' Old environment: `r inputenv_old`
+#' 
+#' New environment: `r inputenv_new`
+oldenv <- new.env(); load(inputenv_old,envir = oldenv);
+newenv <- new.env(); load(inputenv_new,envir = newenv);
+
+#' ### Does every top-leveltable have the same row counts?
+test_rowcounts <- cbind(
+  old=sapply(oldenv,nrow) %>% Filter(Negate(is.null),.) %>% cbind(),
+  new=sapply(newenv,nrow) %>% Filter(Negate(is.null),.) %>% cbind()
+);
+test_rowcounts;
+
+#' ### What columns are different in the old and new data?
+setdiff(names(oldenv$dat0),names(newenv$dat0));
+setdiff(names(newenv$dat0),names(oldenv$dat0));
