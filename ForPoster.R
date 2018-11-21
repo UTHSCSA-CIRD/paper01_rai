7ec749f416f534cfcc288017b66262ab7933e229
Alex F. Bokov <bokov@uthscsa.edu>
2018-11-21 16:39:13 -0600

    ForPoster.R;correction;Updated survival plots for abstract

--- ForPoster.R
+++ ForPoster.R
@@ -223,19 +223,23 @@ fit_srvs_optcut <- list(`RAI-A`=survfit(Surv(a_t,a_c) ~ I(a_rai>9.5)
                                  , data = sbs0$all$all_emergency,subset=a_t>0)
                  ,Rockwood=survfit(Surv(a_t,a_c) ~ I(a_rockwood>0.264245)
                                    , data = sbs0$all$all_emergency,subset=a_t>0));
-pl_srvs <- mapply(function(aa,bb) autoplot(aa,ylim=c(0.5,1),xlim=c(0,30)) + 
-                    ggtitle(paste0(bb,' as a predictor of  death/readmission')) +
-                    scale_y_continuous(labels=scales::percent) +
+pl_srvs <- mapply(function(aa,bb) autoplot(aa) +
+                                           #,ylim=c(0.5,1),xlim=c(0,30),asp=1.45) + 
+                    ggtitle(paste0(bb,' as a predictor of 30-day readmission')) +
+                    scale_y_continuous(labels=scales::percent,limits = c(0.5,1)) +
                     scale_color_discrete(bb,labels=c('Low','High')) +
                     scale_fill_discrete(bb,labels=c('Low','High')) +
-                    labs(x='Days Post-Surgery',y='No Death or Readmission')
+                    labs(x='Days Post-Surgery',y='Readmission-Free Survival') +
+                    theme_light(base_size=19) + 
+                    guides(color=guide_legend(title='')
+                           ,fill=guide_legend(title=''))
                   ,fit_srvs,names(fit_srvs),SIMPLIFY = F);
 pl_srvs_optcut <- mapply(function(aa,bb) autoplot(aa,ylim=c(0.5,1),xlim=c(0,30)) + 
                     ggtitle(paste0(bb,' as a predictor, OPTCUT')) +
                     scale_y_continuous(labels=scales::percent) +
                     scale_color_discrete(bb,labels=c('Low','High')) +
                     scale_fill_discrete(bb,labels=c('Low','High')) +
-                    labs(x='Days Post-Surgery',y='No Death or Readmission')
+                    labs(x='Days Post-Surgery',y='Readmission-Free Survival')
                   ,fit_srvs_optcut,names(fit_srvs),SIMPLIFY = F);
 
 multiplot(plotlist=pl_srvs,cols=1);
